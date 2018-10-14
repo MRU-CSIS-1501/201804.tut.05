1435c9efb2a7dd0faff6f1bd16338c2eeae0d6ea
jpratt-mru <jpratt@mtroyal.ca>
2018-10-14 15:29:41 -0600

    Push.

---
+++ DebuggingExample2.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class DebuggingExample2 {
+
+    private Scanner kbd = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        new DebuggingExample2().run();
+    }
+
+    public void run() {
+        String word = wordFromUser();
+        reportOnVowelsFor(word);
+    }
+
+    private String wordFromUser() {
+        System.out.print("Enter a word - I'll tell you what vowels it has > ");
+        return kbd.nextLine();
+    }
+
+    private void reportOnVowelsFor(String word) {
+        String vowelsFound = vowelsIn(word);
+        if (vowelsFound.isEmpty()) {
+            System.out.println("No vowels there!");
+        } else {
+            System.out.println("Found these vowels: " + vowelsFound);
+        }
+
+    }
+
+    private String vowelsIn(String word) {
+        int length = word.length();
+        String vowelsFound = "";
+
+        for (int i = 0; i < length; i++) {
+            String c = word.substring(i, i + 1);
+            if (isVowel(c) && !vowelsFound.contains(c)) {
+                vowelsFound += c;
+            }
+        }
+        return vowelsFound;
+    }
+
+    private boolean isVowel(String c) {
+        return "AEIOU".contains(c);
+    }
+
+}
